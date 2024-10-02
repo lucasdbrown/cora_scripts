518a15ba123e204522a8c83e178a5a5ca6b826d2
Lukas May <76849604+lukasmay@users.noreply.github.com>
2024-10-02 12:03:26 -0400

    Update network_monitoring.sh

--- blue_team/for_comp/network_monitoring.sh
+++ blue_team/for_comp/network_monitoring.sh
@@ -4,12 +4,10 @@
 
 SUSPICIOUS_PORTS=(21 22 23 445 3389)
 LOG_FILE="/var/log/network_monitor.log"
-ALERT_EMAIL="admin@example.com"
 
 echo "[+] Monitoring network services..."
 for port in "${SUSPICIOUS_PORTS[@]}"; do
     if netstat -tuln | grep ":$port " > /dev/null; then
         echo "[ALERT] Suspicious port $port open on $(hostname)" >> $LOG_FILE
-        mail -s "Suspicious Port Opened" $ALERT_EMAIL < $LOG_FILE
     fi
 done
